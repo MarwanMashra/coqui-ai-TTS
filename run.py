132fd14b3a2cd79bdc765c62b9bb26766fca56ac
Marwan <marwan@jumbomana.com>
2025-06-07 16:54:21 +0200

    minor test update

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from TTS.api import TTS
 def run(output_dir: Path):
     tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to("cuda")
     tts.tts_to_file(
-        text="Greetings.",
-        speaker="Gracie Wise",
+        text="Hello, I would like to welcome you to the world of streaming TTS.",
+        speaker="Ana Florence",
         language="en",
         file_path=str(output_dir / "output.wav"),
     )
